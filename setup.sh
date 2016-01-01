e4a7baafbc823be6e909ec85d9c18a5d30f29c35
Antonios Hadjigeorgalis <Antonios@Mac-Mini.home>
2015-12-31 19:16:56 -0500

    break out git setup for mac

--- setup.sh
+++ setup.sh
@@ -27,25 +27,6 @@ if [ -d $HOME/.vim/bundle/YouCompleteMe/ ]; then
   fi
 fi
 
-# Git Configuration
-sudo apt-get install -y git-core
-git config --global user.email "Antonios@$HOSTNAME"
-git config --global user.name "Antonios Hadjigeorgalis"
-git config --global color.ui true
-# simple is new default push method after Git 2.0
-git config --global push.default simple
-git config --global credential.helper 'cache --timeout=3600'
-git config --global grep.linenumber true
-git config --global grep.extendregexp true
-git config --global alias.g 'grep --break --heading --line-number'
-git config --global push.default simple
-git config --global core.autocrlf input
-git config --global status.branch true
-git config --global status.short true
-# http://haacked.com/archive/2014/07/28/github-flow-aliases/
-# TODO: create aliases for 
-# git clone --recursive (always want to recurse submodules)
-# git pull --recurse-submodules (always want latest submodule)
 
 # Curl Configuration
 ln -sf $HOME/dotfiles/curlrc $HOME/.curlrc
