ba16b8e01ab94c2b160960485dabd159803da46d
Antonios Hadjigeorgalis <Antonios@Mac-Mini.home>
2016-01-09 12:21:47 -0500

    added homebrew to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Bash Configuration
-if [ "$(uname -s)" == 'Linux' ]; then
-  sudo apt-get install -y tree
-fi
 ln -sf $HOME/dotfiles/bash_profile $HOME/.bash_profile
 ln -sf $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/dotfiles/inputrc $HOME/.inputrc
@@ -37,26 +34,28 @@ ln -sf $HOME/dotfiles/curlrc $HOME/.curlrc
 # Tmux Configuration
 ln -sf $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 
-# Terminal Colors Configuration
+# Font Configuration for Airline or Powerline
+$HOME/dotfiles/fonts/install.sh
+
 # Linux Only
 if [ "$(uname -s)" == 'Linux' ]; then
-  # Airline Font Configuration
-  # TODO: this needs further research, or use powerline
-  #trgt=$HOME/dotfiles/config/fontconfig/font.conf/10-powerline-symbols.conf
-  #dest=$HOME/.config/fontconfig/fonts.conf/.
-  #mkdir -p $dest
-  #ln -sf $trgt $dest
-
+  # Terminal Colors Configuration
   ln -sf $HOME/dotfiles/dircolors $HOME/.dircolors
+  # install useful programs 
+  sudo apt-get install -y tree
+  sudo apt-get install -y git
+  sudo apt-get install -y vim
+  sudo apt-get install -y vim-gnome
+fi
 fi
 
 # Mac OSX Only
 if [ "$(uname -s)" == 'Darwin' ]; then
+  # Terminal Colors Configuration
   # TODO: Automatically import and delete. Manual go to Preferences > Profiles
   wget https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
   wget https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Light.itermcolors
+  # install homebrew http://brew.sh/
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-# Font Configuration for Airline or Powerline
-$HOME/dotfiles/fonts/install.sh
 
