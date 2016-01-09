4c2aab14165d81224b9f116c08faaf34327a506f
Antonios Hadjigeorgalis <Antonios@antonios-linux>
2016-01-09 18:52:26 -0500

    updated notes

--- build-instructions/Antonios-VIM-README.md
+++ build-instructions/Antonios-VIM-README.md
@@ -2,7 +2,9 @@ http://www.vim.org/git.php
 
 ## Build Instructions
 cd src  
+# symlink from dotfiles/build-instructions/  
 ./configure-vim.sh  
-make distclean  # if Vim was previously compiled  
+# if Vim was previously compiled  
+make distclean  
 make  
 sudo make install
--- update-all.sh
+++ update-all.sh
@@ -6,13 +6,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 
-# update perl
-# to use cpanm: curl -L http://cpanmin.us | sudo perl - App::cpanminus
-sudo cpanm App::Ack
-sudo cpanm Finance::Quote
-# https://stackoverflow.com/questions/3727795/how-do-i-update-all-my-cpan-module-to-their-latest-versions
-# sudo cpan-outdated -p | sudo cpanm
-
 vim +PluginUpdate +qall
 
 sudo -H pip install --upgrade -r ~/dotfiles/requirements.txt
@@ -22,4 +15,11 @@ cabal update
 cabal install cabal-install
 cabal install --force-reinstall pandoc
 cabal list --installed Pandoc
+
+# update perl
+# to use cpanm: curl -L http://cpanmin.us | sudo perl - App::cpanminus
+sudo cpanm App::Ack
+sudo cpanm Finance::Quote
+# https://stackoverflow.com/questions/3727795/how-do-i-update-all-my-cpan-module-to-their-latest-versions
+# sudo cpan-outdated -p | sudo cpanm
 
