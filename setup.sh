4cb6ad8e60da281a8a2425eb3c501ba4fa7432fb
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-01-11 10:19:13 -0500

    Combine setup files
    Custom emails in git config breaks github commit tracking

--- setup.sh
+++ setup.sh
@@ -70,7 +70,6 @@ vim +PluginInstall +qall
 #  fi
 #fi
 
-
 # Curl Configuration
 ln -sf $HOME/dotfiles/curlrc $HOME/.curlrc
 
@@ -80,4 +79,21 @@ ln -sf $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 # Font Configuration for Airline or Powerline
 $HOME/dotfiles/fonts/install.sh
 
-
+# Git Configuration
+git config --global user.email "Antonios@Hadji.co"
+git config --global user.name "Antonios Hadjigeorgalis"
+git config --global color.ui true
+# simple is new default push method after Git 2.0
+git config --global push.default simple
+git config --global credential.helper 'cache --timeout=3600'
+git config --global grep.linenumber true
+git config --global grep.extendregexp true
+git config --global alias.g 'grep --break --heading --line-number'
+git config --global push.default simple
+git config --global core.autocrlf input
+git config --global status.branch true
+git config --global status.short true
+# http://haacked.com/archive/2014/07/28/github-flow-aliases/
+# TODO: create aliases for 
+# git clone --recursive (always want to recurse submodules)
+# git pull --recurse-submodules (always want latest submodule)
