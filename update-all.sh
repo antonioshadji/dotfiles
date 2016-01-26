9bb748e8eb05600974adcd7034003c369ee370f4
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-01-26 18:19:24 -0500

    test tools before running setup for pandoc, npm

--- update-all.sh
+++ update-all.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+# Linux Only
+if [ "$(uname -s)" != 'Linux' ]; then
+  echo 'this update only for linux machines'
+  exit
+fi
+
 # update linux
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -10,18 +16,24 @@ vim +PluginUpdate +qall
 
 sudo -H pip install --upgrade -r ~/dotfiles/requirements.txt
 
+if command -v cabal; then
 # update pandoc
 cabal update
 cabal install cabal-install
 cabal install --force-reinstall pandoc
 cabal list --installed Pandoc
+fi
 
+# if command -v cpanm; then
 # update perl
 # to use cpanm: curl -L http://cpanmin.us | sudo perl - App::cpanminus
-sudo cpanm App::Ack
-sudo cpanm Finance::Quote
+# sudo cpanm App::Ack
+# sudo cpanm Finance::Quote
 # https://stackoverflow.com/questions/3727795/how-do-i-update-all-my-cpan-module-to-their-latest-versions
 # sudo cpan-outdated -p | sudo cpanm
 
+if command -v npm; then
 # update node
 npm update -g
+fi
+
