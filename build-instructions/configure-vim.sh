a0aefa636f86a2701547aa83948948cee4151494
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-02-02 21:07:27 -0500

    Clean up configure vim script to remove settings that were defaults
    using --features=huge includes some of the other defaults that I had set
    up in the configure script.

--- build-instructions/configure-vim.sh
+++ build-instructions/configure-vim.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+MostCurrentTag=$(git describe --tags)
+git checkout $MostCurrentTag
+make distclean
+
 ./configure \
   --with-features=huge \
-  --enable-pythoninterp=dynamic \
-  --enable-cscope \
-  --enable-gui=auto \
-  --enable-fontset \
+  --enable-pythoninterp=yes \
+  --enable-luainterp=yes \
   --with-compiledby="Antonios Hadjigeorgalis"
+
