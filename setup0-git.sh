42dffd7aabf35b061d31fc4e8000f8c6c9712d40
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-11-21 16:25:27 -0500

    move git config to symlink

--- setup0-git.sh
+++ setup0-git.sh
@@ -2,29 +2,21 @@
 # -*- coding: utf-8 -*-
 
 # Git Configuration
-if [ "$(uname -s)" == "Linux" ]; then
-   sudo apt-get install -y git
+if ! command -v git; then
+  if [ "$(uname -s)" == "Linux" ]; then
+     sudo apt-get install -y git
+  fi
 fi
-
-git config --global user.email "Antonios@Hadji.co"
-git config --global user.name "Antonios Hadjigeorgalis"
-git config --global color.ui true
-# simple is new default push method after Git 2.0
-git config --global push.default simple
-git config --global credential.helper 'cache --timeout=3600'
-git config --global grep.linenumber true
-git config --global grep.extendregexp true
-git config --global core.autocrlf input
-git config --global status.branch true
-git config --global status.short true
-# http://haacked.com/archive/2014/07/28/github-flow-aliases/
-# TODO: create aliases for
-# git clone --recursive (always want to recurse submodules)
-# git pull --recurse-submodules (always want latest submodule)
-git config --global alias.g 'grep --break --heading --line-number'
-git config --global alias.s 'status'
 
 # create dotfiles directory
 if [[ ! -d $HOME/dotfiles ]]; then
   git clone --recursive git@github.com:AntoniosHadji/dotfiles.git $HOME/dotfiles
+  (cd $HOME/dotfiles && git pull --recurse-submodules)
 fi
+
+ln -sf $HOME/dotfiles/gitconfig $HOME/.gitconfig
+
+# http://haacked.com/archive/2014/07/28/github-flow-aliases/
+# TODO: create aliases for
+# git clone --recursive (always want to recurse submodules)
+# git pull --recurse-submodules (always want latest submodule)
