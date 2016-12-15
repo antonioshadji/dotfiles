cf9ff759d70302ddcc6ce56e9fcaa3ded1af728b
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-12-15 17:19:35 -0500

    refactored, added bold-color

--- term-color.sh
+++ term-color.sh
@@ -4,11 +4,27 @@ export DISPLAY=:0
 PID=$(pgrep gnome-terminal)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
+profile_id="$(dconf read /org/gnome/terminal/legacy/profiles:/default|sed s/\'//g)"
+
+# http://ethanschoonover.com/solarized
+# light
+base3='#fdfdf6f6e3e3'  # background
+base00='#65657b7b8383' # body text/default code/primary content
+base01='#58586e6e7575' # optional emphasized content
+# dark
+base03='#00002b2b3636' # background
+base0='#838394949696'  # body text/default code/primary content
+base1='#9393a1a1a1a1'  # optional emphasized content
+
 # http://www.tldp.org/LDP/abs/html/comparison-ops.html
 if [ "$1" = "light" ]; then
-  dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/background-color "'#fdfdf6f6e3e3'"
-  dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/foreground-color "'#65657b7b8383'"
+  dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/background-color "'$base3'"
+  dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/foreground-color "'$base00'"
+  dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/bold-color "'$base01'"
+elif [ "$1" = "dark" ]; then
+  dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/background-color "'$base03'"
+  dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/foreground-color "'$base0'"
+  dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/bold-color "'$base1'"
 else
-  dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/background-color "'#00002B2B3636'"
-  dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/foreground-color "'#838394949696'"
+  echo "Requires parameter of light or dark"
 fi
