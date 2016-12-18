8321b10402e95524fc83b19368fbc3fb43aca25d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-12-18 18:08:27 -0500

    test

--- systemd-test.sh
+++ systemd-test.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-F=/home/antonios/terminal-color.log
-echo "systemd-test.sh" >> $F
-echo $(date) >> $F
-echo $DBUS_SESSION_BUS_ADDRESS >> $F
-echo $DISPLAY >> $F
-echo "systemd-test.sh run completed" >> $F
 
+# Set up environment if necessary
+if [ -z $DISPLAY ]; then
+  export DISPLAY=:0
+fi
+if [ -z $DBUS_SESSION_BUS_ADDRESS ]; then
+  PID=$(pgrep gnome-session)
+  export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+fi
+
+echo "systemd-test.sh" >> $1
+echo $(date) >> $1
+echo $DBUS_SESSION_BUS_ADDRESS >> $1
+echo $DISPLAY >> $1
+echo "systemd-test.sh run completed" >> $1
