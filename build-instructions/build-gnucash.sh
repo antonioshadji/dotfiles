a71f7291d14b48ac65ab328e8ac1c4c498f1d8fc
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-12-22 19:51:43 -0500

    working build script

--- build-instructions/build-gnucash.sh
+++ build-instructions/build-gnucash.sh
@@ -1,39 +1,34 @@
 #!/usr/bin/env bash
 
-make distclean
+# set script to exit if any command fails
+# http://stackoverflow.com/a/90441/2472798
+set -e
+set -o pipefail
+
+# get latest tag from git repo
 git checkout master
 git pull
 REPO=$(git describe --abbrev=0)
 git checkout $REPO
-read -n1 -r -p "Repo updated -- Press space to run autogen..." key
+
+# add variable to restrict language files to only british english
+# this stops building of all languages
+export LINGUAS=en_GB
 
 ./autogen.sh
-read -n1 -r -p "autogen completed -- Press space to create build directory..." key
 
-[[ -d "build_$REPO" ]] && rm -rf "build_$REPO"
-mkdir "build_$REPO"
-cd "build_$REPO"
-export LINGUAS=en
-read -n1 -r -p "build sub-dir created -- Press space to configure..." key
-../configure --enable-python --enable-ofx --enable-aqbanking
-#  --enable-debug --enable-dependency-tracking --enable-compile-warnings=yes
+# Create a separate build directory for each build
+if [[ -d "build_$REPO" ]]; then
+  mkdir "build_$REPO-$(date +%y%m%d%H%M)"
+  cd "build_$REPO-$(date +%y%m%d%H%M)"
+else
+  mkdir "build_$REPO"
+  cd "build_$REPO"
+fi
 
-#  --prefix=/opt/gnucash  install to custom location
-#  --enable-compile-warnings  reccomended in instructions
-#  --enable-python         enable python plugin and bindings
-#  --enable-ofx            compile with ofx support (needs LibOFX)
-#  --enable-aqbanking      compile with AqBanking support
-#  --enable-debug          compile with debugging flags set
-#   --enable-dependency-tracking
-read -n1 -r -p "Configured -- Press space to make..." key
-
-#if [ "$key" = ' ' ]; then
-    # Space pressed, do something
-#else
-    # Anything else pressed, do whatever else.
-#fi
+../configure --prefix=/opt/gnucash --enable-compile-warnings \
+  --enable-python --enable-ofx --enable-aqbanking \
+  --enable-doxygen --enable-html-docs --enable-debug
 
 make
-echo $0
-read -n1 -r -p "Made -- Press space to install..." key
-sudo make install
+sudo checkinstall --pkgname=gnucash --pkgversion=1:$REPO --default
