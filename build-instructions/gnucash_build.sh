297ea56b3cee4ce2a6fc62d3ebbaa5c1a63be9e8
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2019-01-02 20:45:35 -0500

    updated gnucash build for version 3.3+

--- build-instructions/gnucash_build.sh
+++ build-instructions/gnucash_build.sh
@@ -3,4 +3,5 @@
 # these lines cause make check to fail
 # export GTEST_ROOT=/mnt/projects/csdiy/programming-1/c++/googletest/build/googlemock/gtest/
 # export GMOCK_ROOT=/mnt/projects/csdiy/programming-1/c++/googletest/build/googlemock/
-cmake -DWITH_PYTHON=ON -DCMAKE_INSTALL_PREFIX=/opt/gnucash -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+#-DCMAKE_INSTALL_PREFIX=/opt/gnucash34 causes failed install can't find libraries
+cmake -DWITH_PYTHON=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
