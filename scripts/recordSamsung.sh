5f9eeee031aa406edc0375266c1045d539f474cf
Antonios Hadjigeorgalis <antonios@hadji>
2019-01-06 16:24:40 -0500

    BIG CHANGE: deleted old files not used or not relevant

--- scripts/recordSamsung.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-avconv -f x11grab -r 25 -s 1920x1080 -i :0.0+1280,0 -vcodec libx264 -pre lossless_ultrafast -threads 0 video.mkv
--- scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get autoremove -y
