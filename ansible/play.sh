93bb6dbeaeec8331f9f1406ec2f0d98882b618c1
Antonios Hadjigeorgalis <antonios@hadji>
2019-01-21 18:00:44 -0500

    update start script

--- ansible/play.sh
+++ ansible/play.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-ansible-playbook site.yml -i hosts.yml --ask-become-pass
+ansible-playbook site.yml --ask-become-pass "$@"
 
 # ansible-playbook site.yml -i hosts.yml --ask-become-pass --limit hadji.local
+# -i inventory set in ansible.cfg
 # --check dry-run
 # --syntax-check to check syntax, do not execute
 # -f, --fork default number of processes to run is 5.
