3f54e2651bf823391c01896c7285148526d9f5f8
Antonios Hadjigeorgalis <antonios@hadji>
2019-02-09 11:37:08 -0500

    gnome tweaks

--- gnome_tweaks.sh
+++ gnome_tweaks.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
-
+gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.desktop.privacy send-software-usage-stats true
