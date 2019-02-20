62fdb8db8a7398665d242003c7e954b4076f0a39
Antonios Hadjigeorgalis <antonios@hadji>
2019-02-19 19:18:49 -0500

    update vim build

--- build-instructions/configure-vim.sh
+++ build-instructions/configure-vim.sh
@@ -5,44 +5,107 @@ git checkout master
 git pull
 make distclean
 LATEST_TAG=$(git describe --tags)
-echo $LATEST_TAG
+echo "$LATEST_TAG"
 sleep 3s
-git checkout $LATEST_TAG
+git checkout "$LATEST_TAG"
 
 sleep 3s
+
 ./configure \
   --enable-luainterp=dynamic \
   --enable-python3interp=yes \
   --enable-cscope \
   --enable-terminal \
   --enable-autoservername \
-  --enable-multibyte \
   --enable-gui=gtk3 \
   --enable-gnome-check \
+  --disable-rightleft \
+  --disable-arabic \
   --with-compiledby="Antonios Hadjigeorgalis"
 # mzscheme installed with racket-lang racket is correct as per help mzscheme
 # fails to compile.
 #  --enable-mzschemeinterp \
+rc="$?"
 
-if [ $? -eq 0 ]; then
+if [[ $rc -eq 0 ]]; then
   echo "./configure success."
-  read -p "Hit enter to continue building vim."
+  read -r -p "Hit enter to continue building vim."
   sleep 1s
   make
 fi
+rc="$?"
 
-if [ $? -eq 0 ]; then
-  echo "successfully made"
-  sleep 1s
-  sudo make install
-fi
+# if [ $rc -eq 0 ]; then
+#   echo "successfully made"
+#   sleep 1s
+#   sudo make install
+# fi
+#
+# git checkout master
+# sleep 1s
+# vim --version
 
-git checkout master
-sleep 1s
-vim --version
 
 
-
+# `configure' configures this package to adapt to many kinds of systems.
+#
+# Usage: auto/configure [OPTION]... [VAR=VALUE]...
+#
+# To assign environment variables (e.g., CC, CFLAGS...), specify them as
+# VAR=VALUE.  See below for descriptions of some of the useful variables.
+#
+# Defaults for the options are specified in brackets.
+#
+# Configuration:
+#   -h, --help              display this help and exit
+#       --help=short        display options specific to this package
+#       --help=recursive    display the short help of all the included packages
+#   -V, --version           display version information and exit
+#   -q, --quiet, --silent   do not print `checking ...' messages
+#       --cache-file=FILE   cache test results in FILE [disabled]
+#   -C, --config-cache      alias for `--cache-file=config.cache'
+#   -n, --no-create         do not create output files
+#       --srcdir=DIR        find the sources in DIR [configure dir or `..']
+#
+# Installation directories:
+#   --prefix=PREFIX         install architecture-independent files in PREFIX
+#                           [/usr/local]
+#   --exec-prefix=EPREFIX   install architecture-dependent files in EPREFIX
+#                           [PREFIX]
+#
+# By default, `make install' will install all the files in
+# `/usr/local/bin', `/usr/local/lib' etc.  You can specify
+# an installation prefix other than `/usr/local' using `--prefix',
+# for instance `--prefix=$HOME'.
+#
+# For better control, use the options below.
+#
+# Fine tuning of the installation directories:
+#   --bindir=DIR            user executables [EPREFIX/bin]
+#   --sbindir=DIR           system admin executables [EPREFIX/sbin]
+#   --libexecdir=DIR        program executables [EPREFIX/libexec]
+#   --sysconfdir=DIR        read-only single-machine data [PREFIX/etc]
+#   --sharedstatedir=DIR    modifiable architecture-independent data [PREFIX/com]
+#   --localstatedir=DIR     modifiable single-machine data [PREFIX/var]
+#   --runstatedir=DIR       modifiable per-process data [LOCALSTATEDIR/run]
+#   --libdir=DIR            object code libraries [EPREFIX/lib]
+#   --includedir=DIR        C header files [PREFIX/include]
+#   --oldincludedir=DIR     C header files for non-gcc [/usr/include]
+#   --datarootdir=DIR       read-only arch.-independent data root [PREFIX/share]
+#   --datadir=DIR           read-only architecture-independent data [DATAROOTDIR]
+#   --infodir=DIR           info documentation [DATAROOTDIR/info]
+#   --localedir=DIR         locale-dependent data [DATAROOTDIR/locale]
+#   --mandir=DIR            man documentation [DATAROOTDIR/man]
+#   --docdir=DIR            documentation root [DATAROOTDIR/doc/PACKAGE]
+#   --htmldir=DIR           html documentation [DOCDIR]
+#   --dvidir=DIR            dvi documentation [DOCDIR]
+#   --pdfdir=DIR            pdf documentation [DOCDIR]
+#   --psdir=DIR             ps documentation [DOCDIR]
+#
+# X features:
+#   --x-includes=DIR    X include files are in DIR
+#   --x-libraries=DIR   X library files are in DIR
+#
 # Optional Features:
 #   --disable-option-checking  ignore unrecognized --enable/--with options
 #   --disable-FEATURE       do not include FEATURE (same as --enable-FEATURE=no)
@@ -62,12 +125,14 @@ vim --version
 #   --enable-tclinterp=OPTS      Include Tcl interpreter. default=no OPTS=no/yes/dynamic
 #   --enable-rubyinterp=OPTS     Include Ruby interpreter.  default=no OPTS=no/yes/dynamic
 #   --enable-cscope         Include cscope interface.
-#   --enable-workshop       Include Sun Visual Workshop support.
 #   --disable-netbeans      Disable NetBeans integration support.
 #   --disable-channel       Disable process communication support.
 #   --enable-terminal       Enable terminal emulation support.
 #   --enable-autoservername Automatically define servername at vim startup.
 #   --enable-multibyte      Include multibyte editing support.
+#   --disable-rightleft     Do not include Right-to-Left language support.
+#   --disable-arabic        Do not include Arabic language support.
+#   --disable-farsi         Deprecated.
 #   --enable-hangulinput    Include Hangul input support.
 #   --enable-xim            Include XIM input support.
 #   --enable-fontset        Include X fontset output support.
@@ -87,7 +152,7 @@ vim --version
 #   --disable-gpm           Don't use gpm (Linux mouse daemon).
 #   --disable-sysmouse      Don't use sysmouse (mouse in *BSD console).
 #   --disable-nls           Don't support NLS (gettext()).
-# 
+#
 # Optional Packages:
 #   --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
 #   --without-PACKAGE       do not use PACKAGE (same as --with-PACKAGE=no)
@@ -98,7 +163,7 @@ vim --version
 #   --with-vim-name=NAME    what to call the Vim executable
 #   --with-ex-name=NAME     what to call the Ex executable
 #   --with-view-name=NAME   what to call the View executable
-#   --with-global-runtime=DIR    global runtime directory in 'runtimepath'
+#   --with-global-runtime=DIR    global runtime directory in 'runtimepath', comma-separated for multiple directories
 #   --with-modified-by=NAME       name of who modified a release version
 #   --with-features=TYPE    tiny, small, normal, big or huge (default: huge)
 #   --with-compiledby=NAME  name to show in :version message
@@ -117,3 +182,19 @@ vim --version
 #   --with-gnome            Specify prefix for GNOME files
 #   --with-motif-lib=STRING Library for Motif
 #   --with-tlib=library     terminal library to be used
+#
+# Some influential environment variables:
+#   CC          C compiler command
+#   CFLAGS      C compiler flags
+#   LDFLAGS     linker flags, e.g. -L<lib dir> if you have libraries in a
+#               nonstandard directory <lib dir>
+#   LIBS        libraries to pass to the linker, e.g. -l<library>
+#   CPPFLAGS    (Objective) C/C++ preprocessor flags, e.g. -I<include dir> if
+#               you have headers in a nonstandard directory <include dir>
+#   CPP         C preprocessor
+#   XMKMF       Path to xmkmf, Makefile generator for X Window System
+#
+# Use these variables to override the choices made by `configure' or to help
+# it to find libraries and programs with nonstandard names/locations.
+#
+# Report bugs to the package provider.
