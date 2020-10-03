3788e974e906ee7bb10c2ea789e57eef1012a35f
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2020-10-03 12:06:36 -0400

    add script for global npm installs

---
+++ npm.packages.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+npm install -g eslint
+npm install -g eslint-config-prettier
+npm install -g neovim
+npm install -g npm
+npm install -g prettier
+npm install -g prettier-eslint
+npm install -g standard
+npm install -g vim-language-server
+
+npm -g list --depth 0
