370923efcbabb8269d7ac1fd3dd0a241fbb53be8
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-04-22 08:44:41 -0400

    Git: recurse submodules
    Add notes and set fetchRecurseSubmodules for jedi which uses several
    layers of nested submodules.

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+# Update all submodules to latest master
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
+
+# update all nested submodules to git committed version
+# git submodule update --recursive
+# Or, set fetchRecurseSubmodules in .gitmodules
