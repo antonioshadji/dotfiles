2e021ce4a7f9c2d814fb0e2ad87e84c6485e0a5a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-04-22 19:27:26 -0400

    setup: updates for latest work desktop

--- bootstrap-primary.sh
+++ bootstrap-primary.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
+# install git, connect to git to clone dotfiles
+# sudo apt update unecessary after add-apt-repository
 if [[ ! -r ~/.ssh/id_ed25519.pub ]]; then
-  ssh-keygen -t ed25519
+  ssh-keygen -t ed25519 -C $USER@$HOSTNAME 
 else
   echo "ed25519 key has already been generated"
 fi
-# TODO: upload new ssh to github using api
 sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:jonathonf/vim
-# sudo apt update unecessary, above lines update by default
 sudo apt install git 
+# TODO: upload new ssh to github using api
+# only do this step if folder does not exist
+# git clone --recurse-submodules git@github.com:AntoniosHadji/dotfiles.git .dotfiles
+
+# other software
+sudo add-apt-repository ppa:jonathonf/vim
 sudo apt install vim 
 sudo apt install python3-distutils
 wget https://bootstrap.pypa.io/get-pip.py 
 python3 get-pip.py --user
 ~/.local/bin/pip install --user ansible
-git clone --recurse-submodules git@github.com:AntoniosHadji/dotfiles.git .dotfiles
+# now ansible can be ran to install symlinks
+# ~/.local/bin/ansible-playbook ansible/playbook.yml --limit "$(hostname).local"
