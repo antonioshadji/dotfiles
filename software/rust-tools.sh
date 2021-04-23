a9e71493fdccada1b7296d7a0e1e0a5812365444
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-04-23 11:55:55 -0400

    rust: man page updates, desktop files for alacritty

--- software/rust-tools.sh
+++ software/rust-tools.sh
@@ -18,3 +18,23 @@ ERR=$?; [[ ! $ERR ]] && exit $ERR
 # only needed on desktop machines
 sudo apt install -y libfontconfig1-dev libx11-xcb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 cargo install alacritty
+
+# man page
+sudo mkdir -p /usr/local/share/man/man1
+URL=https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.man
+FILE=/usr/local/share/man/man1/alacritty.1.gz
+sudo rm "$FILE"
+curl "$URL" | gzip -c | sudo tee "$FILE" > /dev/null
+
+# desktop file
+URL=https://raw.githubusercontent.com/alacritty/alacritty/master/extra/logo/alacritty-term.svg
+FILE=/usr/share/pixmaps/Alacritty.svg
+if [[ ! -f $FILE ]]; then
+  sudo curl "$URL" --output "$FILE"
+
+  URL=https://raw.githubusercontent.com/alacritty/alacritty/master/extra/linux/Alacritty.desktop
+  curl -O "$URL"
+  sudo desktop-file-install --delete-original ./Alacritty.desktop
+  # rm -f Alacritty.desktop
+  sudo update-desktop-database
+fi
