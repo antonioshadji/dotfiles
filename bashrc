93fd3aab7e742b6b8f296384bfd3dfea4090a161
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-04-23 08:38:39 -0400

    rust: update install script and bashrc setup

--- bashrc
+++ bashrc
@@ -298,7 +298,7 @@ unset gemloc
 [[ -d $HOME/.local/bin ]] && export PATH=$HOME/.local/bin:$PATH
 
 # Rust
-[[ -d $HOME/.cargo/bin ]] && export PATH="$HOME/.cargo/bin:$PATH"
+[[ -d $HOME/.cargo ]] && source "$HOME/.cargo/env"
 
 # Golang
 if [[ ! "$PATH" == */usr/local/go/bin* ]]; then
--- software/rust-tools.sh
+++ software/rust-tools.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 # requires build tools to be installed first
-sudo apt install curl gcc make cmake g++
+sudo apt install -y curl gcc make cmake g++
 if [[ ! -d ~/.cargo ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
-source $HOME/.cargo/env
+ERR=$?; [[ ! $ERR ]] && exit $ERR
+
+# shellcheck source=/dev/null
+source ~/.cargo/env
+
 cargo install fd-find
+ERR=$?; [[ ! $ERR ]] && exit $ERR
 cargo install ripgrep
+ERR=$?; [[ ! $ERR ]] && exit $ERR
 
 # only needed on desktop machines
-sudo apt install libfontconfig1-dev libx11-xcb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+sudo apt install -y libfontconfig1-dev libx11-xcb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 cargo install alacritty
