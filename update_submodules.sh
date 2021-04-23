352edc6a346bc47f8fdfb516448e9520e324c7e2
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-04-23 17:28:09 -0400

    git: update submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -4,8 +4,12 @@
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
 
-cd nvm || echo "Failed to find nvm folder."
+cd nvm || echo "Failed to find nvm folder."; exit 1
 git checkout "$(git describe --abbrev=0 --tags --match 'v[0-9]*')"
+cd ..
+
+cd fzf || echo "Failed to find fzf folder."; exit 2
+git checkout "$(git describe --abbrev=0 --tags)"
 
 # update all nested submodules to git committed version
 # git submodule update --recursive
