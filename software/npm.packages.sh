360287c01e9e8dc738ea67929750affd27924c74
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-04-29 10:28:18 -0400

    node: update global packages ;

--- software/npm.packages.sh
+++ software/npm.packages.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+# base install
 npm install -g npm
 npm install -g eslint
 npm install -g prettier
 npm install -g prettier-eslint
 npm install -g eslint-config-prettier
-npm install -g standard
-
+# vim/neovim
 npm install -g neovim
 npm install -g vim-language-server
+# primetrust
+npm install -g @apidevtools/swagger-cli
 
 npm -g list --depth 0
