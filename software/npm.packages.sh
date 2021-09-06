fb1ae7b4d758640982a413eb0a862184cc0bdea5
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-09-06 10:08:04 -0400

    update submodules

--- software/npm.packages.sh
+++ software/npm.packages.sh
@@ -5,6 +5,8 @@ npm install -g eslint
 npm install -g prettier
 npm install -g prettier-eslint
 npm install -g eslint-config-prettier
+npm install -g typescript
+npm install -g yarn
 # vim/neovim
 npm install -g neovim
 npm install -g vim-language-server
