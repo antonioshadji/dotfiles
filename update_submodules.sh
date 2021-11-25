d4e47e10b18bdea333bee8359f9c31ea2793cead
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-11-25 17:24:52 -0500

    update submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -4,6 +4,10 @@
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
 
+cd vim/pack/plugins/start/jedi-vim  || echo "Failed to find jedi-vim"; exit 3
+git submodule update
+cd ..
+
 cd nvm || echo "Failed to find nvm folder."; exit 1
 git checkout "$(git describe --abbrev=0 --tags --match 'v[0-9]*')"
 cd ..
@@ -11,6 +15,5 @@ cd ..
 cd fzf || echo "Failed to find fzf folder."; exit 2
 git checkout "$(git describe --abbrev=0 --tags)"
 
-# update all nested submodules to git committed version
-# git submodule update --recursive
-# Or, set fetchRecurseSubmodules in .gitmodules
+# update submodules to git committed version
+# git submodule update
