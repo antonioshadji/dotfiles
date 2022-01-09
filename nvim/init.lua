dee48dbbd252c97a9730a2295f8ba6ac580b76e3
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-01-09 16:35:15 -0500

    vim: rust config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,12 @@ set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vim/vimrc
 ]])
+
+vim.cmd('packadd nvim-lspconfig')
+-- TODO move to file type loading
+vim.cmd('packadd rust-tools.nvim')
+require('rust-tools').setup({})
+
+vim.cmd([[
+set omnifunc=v:lua.vim.lsp.omnifunc
+]])
