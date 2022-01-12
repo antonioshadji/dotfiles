d021958c3298e42aaaed4ec03cb2d19d110cc58f
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-01-11 19:12:10 -0500

    go: updater updated for new web page

--- software/golang.py
+++ software/golang.py
@@ -11,32 +11,50 @@ from getpass import getpass
 import requests
 from lxml import html
 
-BASE = "https://golang.org"
+BASE = "https://go.dev"
 
-r = requests.get(BASE + "/dl/")
-tree = html.fromstring(r.content)
-url = tree.xpath("/html/body/main/div/a[3]")[0].attrib["href"]
-fn = url.split("/")[-1]
-if not re.match(r"go1.*linux-amd64.tar.gz", fn):
-    print("Filename does not match expected pattern.")
-    exit(1)
 
-r = requests.get(BASE + url, allow_redirects=True)
-open(fn, "wb").write(r.content)
+def get_content(url):
+    r = requests.get(BASE + "/dl/")
+    tree = html.fromstring(r.content)
+    return tree
 
-cmd0 = "sudo rm -rf /usr/local/go".split()
-cmd1 = "sudo tar -C /usr/local -xzf".split()
-cmd1.append(fn)
 
-pw = getpass("sudo password: ")
+def find_file(tree):
+    for link in tree.xpath("/html/body/main/article/a"):
+        if "linux" in link.xpath("@href")[0]:
+            return link.xpath("@href")[0]
 
-subprocess.run(["go", "version"])
 
-proc = subprocess.run(cmd0, capture_output=True, input=pw, encoding="ascii")
-print(proc)
-if proc.returncode == 0:
-    proc = subprocess.run(cmd1, capture_output=True, input=pw, encoding="ascii")
+def main():
+    tree = get_content(BASE + "/dl/")
+    url = find_file(tree)
+
+    fn = url.split("/")[-1]
+    if not re.match(r"go1.*linux-amd64.tar.gz", fn):
+        print("Filename does not match expected pattern.")
+        exit(1)
+
+    r = requests.get(BASE + url, allow_redirects=True)
+    open(fn, "wb").write(r.content)
+
+    cmd0 = "sudo rm -rf /usr/local/go".split()
+    cmd1 = "sudo tar -C /usr/local -xzf".split()
+    cmd1.append(fn)
+
+    pw = getpass("sudo password: ")
+
+    subprocess.run(["go", "version"])
+
+    proc = subprocess.run(cmd0, capture_output=True, input=pw, encoding="ascii")
     print(proc)
-    os.remove(fn)
+    if proc.returncode == 0:
+        proc = subprocess.run(cmd1, capture_output=True, input=pw, encoding="ascii")
+        print(proc)
+        os.remove(fn)
 
-subprocess.run(["go", "version"])
+    subprocess.run(["go", "version"])
+
+
+if __name__ == "__main__":
+    main()
