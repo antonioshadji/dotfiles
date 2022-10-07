152cdba857a905615a9a35f14e2b7f202c36a004
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-10-07 17:44:24 -0400

    gnucash: build scripts

--- build-instructions/create_build_files_ninja.sh
+++ build-instructions/create_build_files_ninja.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-export GTEST_ROOT=/mnt/projects/Environment/software/googletest
+export GTEST_ROOT=/mnt/projects/software/googletest
 # options found in CMakeList.txt in toplevel src directory
 cmake \
   -D WITH_PYTHON=ON \
   -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+  -GNinja \
   ../gnucash
+#  "$(dirname "$(pwd)")/gnucash"
 
-
-# other options
-# -D CMAKE_INSTALL_PREFIX=/opt \
-# -G Ninja
+# compiles successfully
+#   -D CMAKE_INSTALL_PREFIX=/opt \
