96483bf5e7c543be79bcdad7144ca738d85f2908
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2023-08-23 16:29:31 -0400

    nvim/i3 updates fzf v0.42

--- nvim/init.lua
+++ nvim/init.lua
@@ -179,3 +179,7 @@ vim.api.nvim_set_keymap("t", "<C-h>", [[<C-\><C-n><C-w>h]], { noremap = true})
 vim.api.nvim_set_keymap("t", "<C-j>", [[<C-\><C-n><C-w>j]], { noremap = true})
 vim.api.nvim_set_keymap("t", "<C-k>", [[<C-\><C-n><C-w>k]], { noremap = true})
 vim.api.nvim_set_keymap("t", "<C-l>", [[<C-\><C-n><C-w>l]], { noremap = true})
+
+
+-- Limelight let g:limelight_conceal_ctermfg = 'darkgray'
+vim.g.limelight_conceal_ctermfg = 237
