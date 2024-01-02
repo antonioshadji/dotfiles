262719eebbc73589183df3a65c48347abdf43fd4
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-01-02 17:47:51 -0500

    vim: all plugins on master/main

--- update_submodules.sh
+++ update_submodules.sh
@@ -6,23 +6,23 @@ NC='\033[0m' # No Color#
 # Update all submodules to latest master
 git submodule update --recursive --remote
 
-(cd ./bash-git-prompt && git checkout "$(git describe --abbrev=0 --tags)")
-echo -e "${GREEN}completed bash-git-prompt update.${NC}"
-
-(cd ./fzf && git checkout "$(git describe --abbrev=0 --tags)")
-echo -e "${GREEN}completed fzf update.${NC}"
-
-(cd ./nvm && git checkout "$(git describe --abbrev=0 --tags)")
-echo -e "${GREEN}completed nvm update.${NC}"
-
-(cd ./tmux/tmux-resurrect && git checkout "$(git describe --abbrev=0 --tags)" && git submodule update)
-echo -e "${GREEN}completed tmux-resurrect update.${NC}"
-
-(cd ./vim/pack/plugins/start/jedi-vim && git checkout "$(git describe --abbrev=0 --tags)")
-(cd ./vim/pack/plugins/start/jedi-vim/pythonx/jedi && git submodule update)
-(cd ./vim/pack/plugins/start/jedi-vim/pythonx/parso && git submodule update)
-(cd ./vim/pack/plugins/start/jedi-vim && git submodule update)
-echo -e "${GREEN}completed jedi-vim update.${NC}"
+# (cd ./bash-git-prompt && git checkout "$(git describe --abbrev=0 --tags)")
+# echo -e "${GREEN}completed bash-git-prompt update.${NC}"
+#
+# (cd ./fzf && git checkout master)
+# echo -e "${GREEN}completed fzf update.${NC}"
+#
+# (cd ./nvm && git checkout "$(git describe --abbrev=0 --tags)")
+# echo -e "${GREEN}completed nvm update.${NC}"
+#
+# (cd ./tmux/tmux-resurrect && git checkout "$(git describe --abbrev=0 --tags)" && git submodule update)
+# echo -e "${GREEN}completed tmux-resurrect update.${NC}"
+#
+# (cd ./vim/pack/plugins/start/jedi-vim && git checkout "$(git describe --abbrev=0 --tags)")
+# (cd ./vim/pack/plugins/start/jedi-vim/pythonx/jedi && git submodule update)
+# (cd ./vim/pack/plugins/start/jedi-vim/pythonx/parso && git submodule update)
+# (cd ./vim/pack/plugins/start/jedi-vim && git submodule update)
+# echo -e "${GREEN}completed jedi-vim update.${NC}"
 
 echo "================================================================================"
 git submodule status
