2899385ee958712132e87e0109463f46c25979ac
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-01-04 12:57:47 -0500

    vim: scan submodules

--- submodules_check_branch.sh
+++ submodules_check_branch.sh
@@ -1,9 +1,10 @@
-T=vim/pack/plugins/opt
+T=vim/pack/plugins/$1
 for d in $(ls -1 $T);
 do
   (
-  cd $T/$d/ &&
+  cd "$T/$d/" &&
   echo "$d" &&
-  git branch && git symbolic-ref --short HEAD
+  # git branch && git symbolic-ref --short HEAD
+  printf "%s %s\n" "$(git branch)" "$(git symbolic-ref --short HEAD)"
   )
 done
