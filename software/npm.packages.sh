4bc04ced4fd70604b21d849fda16b0aa74548709
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-01-26 11:59:33 -0500

    node: update to 20 LTS

--- software/npm.packages.sh
+++ software/npm.packages.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# NOTE: yarn is part of corepack installed by default in latest nodejs
+
 # base install
 npm install -g npm
 npm install -g eslint
@@ -6,33 +9,37 @@ npm install -g prettier
 npm install -g prettier-eslint
 npm install -g eslint-config-prettier
 npm install -g typescript
-# vim/neovim
+
+# neovim
 npm install -g neovim
-npm install -g vim-language-server
-# primetrust
-npm install -g @stoplight/spectral-cli
+npm install -g typescript-language-server
+npm install -g @nomicfoundation/solidity-language-server
+
+# tools
+npm install -g pdfkit
+npm install -g puppeteer
+
+
 
 npm -g list --depth 0
 
-# yarn installed separately after node 18.6
-# https://yarnpkg.com/getting-started/install
-# npm install -g yarn
 
-# 2022-12-31 10:16:24 installed in previous version globally
-# /home/antonios/.config/dotfiles/nvm/versions/node/v16.15.0/lib
-# ├── @iboss/terrain@0.0.8
-# ├── @openapitools/openapi-generator-cli@2.5.2
-# ├── @redocly/openapi-cli@1.0.0-beta.65
-# ├── @stoplight/spectral@5.9.2
-# ├── corepack@0.10.0
-# ├── eslint-config-prettier@8.3.0
-# ├── eslint@7.32.0
-# ├── express-generator@4.16.1
+# 2024-01-26 11:04:12
+# /home/antonios/.config/dotfiles/nvm/versions/node/v18.12.1/lib
+# ├── @nomicfoundation/solidity-language-server@0.7.3
+# ├── @stoplight/spectral-cli@6.11.0
+# ├── corepack@0.14.2
+# ├── eslint-config-prettier@9.1.0
+# ├── eslint@8.56.0
 # ├── neovim@4.10.1
-# ├── npm@9.2.0
-# ├── prettier-eslint@13.0.0
-# ├── prettier@2.3.2
-# ├── redoc-cli@0.13.0
-# ├── typescript@4.4.2
-# ├── vim-language-server@2.2.5
-# └── yarn@1.22.19
+# ├── nmp@1.0.3
+# ├── npm@9.8.1
+# ├── pdfkit@0.14.0
+# ├── prettier-eslint@16.2.0
+# ├── prettier@3.1.1
+# ├── puppeteer@21.1.1
+# ├── typescript-language-server@4.2.0
+# ├── typescript@5.3.3
+# ├── vim-language-server@2.3.1
+# └── web3@1.8.1
+#
