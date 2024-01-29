d9db4cf8b743c391b4c1a9e443dc54af37ee6175
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-01-29 10:46:39 -0500

    nvim: add lua lang server

--- nvim/init.lua
+++ nvim/init.lua
@@ -539,15 +539,16 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 --     capabilities = capabilities
 --   }
 
-require("lspconfig").tsserver.setup({
-	capabilities = capabilities,
-})
 require("lspconfig").jedi_language_server.setup({
 	capabilities = capabilities,
 })
 -- require("lspconfig").pyright.setup({
 -- capabilities = capabilities,
 -- })
+
+require("lspconfig").tsserver.setup({
+	capabilities = capabilities,
+})
 
 require("lspconfig").gopls.setup({
 	capabilities = capabilities,
@@ -560,9 +561,41 @@ require("lspconfig").clangd.setup({
 	capabilities = capabilities,
 })
 
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+			client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+				Lua = {
+					runtime = {
+						-- Tell the language server which version of Lua you're using
+						-- (most likely LuaJIT in the case of Neovim)
+						version = "LuaJIT",
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						},
+						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					},
+				},
+			})
+
+			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+		end
+		return true
+	end,
+})
+
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "python", "javascript", "go", "rust", "cpp", "sql", "c", "lua", "vim", "vimdoc", "query" },
+	ensure_installed = { "python", "javascript", "go", "rust", "cpp", "c", "lua", "vim", "vimdoc", "query", "sql" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
