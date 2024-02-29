7702bfb39b5274e80ed23dae227efdefc475ffed
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-02-29 10:48:58 -0500

    nvim: update script

--- software/update_nvim.sh
+++ software/update_nvim.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+nvim --version
 curl --location --remote-name https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar --extract --gzip --verbose --file=nvim-linux64.tar.gz   # -xzvf
+tar --extract --gzip --file=nvim-linux64.tar.gz   # -xzf
 sudo rsync --archive ./nvim-linux64/ /usr/
 rm -rf ./nvim-linux64/ nvim-linux64.tar.gz
+nvim --version
 # sudo rsync --archive ./nvim-linux64/ debian:/usr/
 # sudo rsync --archive ./nvim-linux64/ ubuntu12:/usr/
 # rm -rf ./nvim-linux64
