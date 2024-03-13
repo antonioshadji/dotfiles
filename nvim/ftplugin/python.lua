a1c0b68adecbd0f0451507562ef5e01d9bcf083d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-03-13 10:33:42 -0400

    nvim: remove python ftplugin

--- nvim/ftplugin/python.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd([[
-packadd jedi-vim
-
-let g:jedi#smart_auto_mappings = 1
-let g:jedi#show_call_signatures_delay = 200
-let g:jedi#use_splits_not_buffers = 'right'
-let g:jedi#goto_command = 'gd'
-
-]])
