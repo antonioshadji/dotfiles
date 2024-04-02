81948f021be3a2c8096f26f94f8882a461867b24
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-04-02 10:09:57 -0400

    python: updates

--- build-instructions/update_python_bindings.sh
+++ build-instructions/update_python_bindings.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Must include version number in #.# format as argument'
+    exit 0
+fi
+if [ ! -d "./build_v${1}" ] 
+then
+  echo "Build directory ./build_v${1} not found."
+  exit 0
+fi
+
 PYTHON="python3.10"
-GNUCASH="5.3"
+GNUCASH="${1}"
+
 mkdir -p "${HOME}/.local/lib/${PYTHON}/site-packages/gnucash"
-cp --recursive "./build_v${GNUCASH}/lib/python3/dist-packages/gnucash" "${HOME}/.local/lib/${PYTHON}/site-packages"
+rm -rf "${HOME}/.local/lib/${PYTHON}/site-packages/gnucash/__pycache__/"
+cp --recursive --verbose "./build_v${GNUCASH}/lib/${PYTHON}/site-packages/gnucash" "${HOME}/.local/lib/${PYTHON}/site-packages"
