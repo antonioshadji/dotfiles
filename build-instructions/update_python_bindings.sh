6ba9c190948bc9f661e0e21c8a7a65ee7a40da8b
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-12-31 19:42:29 -0500

    gnucash: update python

--- build-instructions/update_python_bindings.sh
+++ build-instructions/update_python_bindings.sh
@@ -5,15 +5,14 @@ if [[ $# -eq 0 ]] ; then
     echo 'Must include version number in #.# format as argument'
     exit 0
 fi
-if [ ! -d "./build_v${1}" ] 
+if [ ! -d "./build_v${1}_stable" ] 
 then
-  echo "Build directory ./build_v${1} not found."
+  echo "Build directory ./build_v${1}_stable not found."
   exit 0
 fi
 
 PYTHON="python3.12"
 GNUCASH="${1}"
 
-mkdir -p "${HOME}/.local/lib/${PYTHON}/site-packages/gnucash"
-rm -rf "${HOME}/.local/lib/${PYTHON}/site-packages/gnucash/__pycache__/"
-cp --recursive --verbose "./build_v${GNUCASH}/lib/${PYTHON}/site-packages/gnucash" "${HOME}/.local/lib/${PYTHON}/site-packages"
+rm -rf "/usr/local/lib/${PYTHON}/site-packages/gnucash"
+cp --recursive --verbose "./build_v${GNUCASH}_stable/lib/${PYTHON}/site-packages/gnucash" "/usr/local/lib/${PYTHON}/site-packages"
