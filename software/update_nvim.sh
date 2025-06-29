29e7395323ce6c713d9ee0ca609dded714792886
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2025-06-29 15:29:12 -0400

    terribly broke system with previous bad update

--- software/update_nvim.sh
+++ software/update_nvim.sh
@@ -3,11 +3,11 @@ filename=nvim-linux-x86_64
 nvim --version
 curl --location --remote-name "https://github.com/neovim/neovim/releases/latest/download/${filename}.tar.gz"
 tar --extract --gzip --file="${filename}.tar.gz"   # -xzf
-# to prevent changes, not needed with --delete to rsync
+# 2025-06-28 --delete on rsync deleted whole /usr directory 
 # sudo rm -rf /usr/share/nvim/runtime
-sudo rsync --archive --delete "./${filename}/" /usr/
-sleep 1
-rm -rf ${filename}*
-nvim --version
+# sudo rsync --archive "./${filename}/" /usr/local/
+# sleep 1
+# rm -rf ${filename}*
+# nvim --version
 # sudo rsync --archive ./nvim-linux64/ debian:/usr/
 # sudo rsync --archive ./nvim-linux64/ ubuntu12:/usr/
