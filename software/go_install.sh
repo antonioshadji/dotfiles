cf35cb053a9e308cfdad051fe4927976c2dd3ba5
Antonios Hadjigeorgalis <ahadjigeorgalis@anaconda.com>
2025-07-15 20:01:09 -0400

    merge from main

---
+++ software/go_install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+go install github.com/junegunn/fzf@latest
+go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
+go install github.com/noborus/ov@latest
