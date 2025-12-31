86cae5b520642bfa0484bcda84107898623dc604
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2025-12-31 17:13:37 -0500

    nvim: update multi-OS

--- software/update_nvim.sh
+++ software/update_nvim.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
-filename=nvim-linux-x86_64
+OS="$(uname -s)"
+
+if [[ "$OS" == "Darwin" ]]; then
+  # Do something for macOS
+  echo "Running on macOS"
+  filename=nvim-macos-arm64
+elif [[ "$OS" == "Linux" ]]; then
+  # Do something for Linux
+  echo "Running on Linux"
+  filename=nvim-linux-x86_64
+else
+    echo "Unknown Operating System: $OS"
+    exit 1
+fi
+
 nvim --version
+
 curl --location --remote-name "https://github.com/neovim/neovim/releases/latest/download/${filename}.tar.gz"
 tar --extract --gzip --file="${filename}.tar.gz"   # -xzf
-# 2025-06-28 --delete on rsync deleted whole /usr directory 
-# sudo rm -rf /usr/share/nvim/runtime
-# sudo rsync --archive "./${filename}/" /usr/local/
-# sleep 1
-# rm -rf ${filename}*
-# nvim --version
-# sudo rsync --archive ./nvim-linux64/ debian:/usr/
-# sudo rsync --archive ./nvim-linux64/ ubuntu12:/usr/
+sudo rm -rf /opt/nvim-*
+sudo mv "${filename}/" /opt/.
+rm "${filename}.tar.gz"
+
+nvim --version
