7d271d497cf680483a5679869a41695ce7898562
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2026-01-01 13:40:55 -0500

    npm: use bash array for global installs

--- software/npm.packages.sh
+++ software/npm.packages.sh
@@ -1,47 +1,32 @@
 #!/usr/bin/env bash
 
 # NOTE: yarn is part of corepack installed by default in latest nodejs
-
-# base install
-npm install -g npm
-npm install -g eslint
-npm install -g prettier
-npm install -g @fsouza/prettierd
-npm install -g eslint-config-prettier
-npm install -g typescript
+package_list=(
+ npm
+ eslint
+ prettier
+ @fsouza/prettierd
+ eslint-config-prettier
+ typescript
 
 # neovim
-npm install -g neovim
-npm install -g typescript-language-server
-npm install -g pyright
-npm install -g bash-language-server
+ neovim
+ typescript-language-server
+ pyright
+ bash-language-server
 
 # tools
-npm install -g pdfkit
-npm install -g puppeteer
-npm install -g @google/gemini-cli
-npm install -g @anthropic-ai/claude-code
-npm install -g wrangler  # cloudflare
+ pdfkit
+ puppeteer
+ @google/gemini-cli
+ @anthropic-ai/claude-code
+ wrangler  # cloudflare
+)
+
+for pkg in "${package_list[@]}"; do
+  echo "Installing: ${pkg}"
+  npm install -g "${pkg}"
+  echo "===="
+done
 
 npm -g list --depth 0
-
-
-# 2024-01-26 11:04:12
-# /home/antonios/.config/dotfiles/nvm/versions/node/v18.12.1/lib
-# ├── @nomicfoundation/solidity-language-server@0.7.3
-# ├── @stoplight/spectral-cli@6.11.0
-# ├── corepack@0.14.2
-# ├── eslint-config-prettier@9.1.0
-# ├── eslint@8.56.0
-# ├── neovim@4.10.1
-# ├── nmp@1.0.3
-# ├── npm@9.8.1
-# ├── pdfkit@0.14.0
-# ├── prettier-eslint@16.2.0
-# ├── prettier@3.1.1
-# ├── puppeteer@21.1.1
-# ├── typescript-language-server@4.2.0
-# ├── typescript@5.3.3
-# ├── vim-language-server@2.3.1
-# └── web3@1.8.1
-#
