f2f565ba2f1600d483e0ad6ad868f806a6470193
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2026-01-04 17:08:27 -0500

    go: install fabric (dm)

--- software/go_install.sh
+++ software/go_install.sh
@@ -3,3 +3,5 @@
 go install github.com/junegunn/fzf@latest
 go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
 go install github.com/noborus/ov@latest
+# Install Fabric directly from the repo
+go install github.com/danielmiessler/fabric/cmd/fabric@latest
