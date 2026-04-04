3a56ffb9f1b51b6114733523695e9131098ba4c5
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2026-04-04 12:17:08 -0400

    go: print message for each install

--- software/go_install.sh
+++ software/go_install.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+printf "\tfzf:"
 go install github.com/junegunn/fzf@latest
+printf "\tdocker-language-server:"
 go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
+printf "\tov"
 go install github.com/noborus/ov@latest
 #
-# Install Fabric directly from the repo
+printf "\tInstall Fabric directly from the repo"
 go install github.com/danielmiessler/fabric/cmd/fabric@latest
 
-# kubernetes tools
+printf "\tkubernetes tools:"
 # NOTE: The dev version will be in effect!
+printf "\tk9s:"
 go install github.com/derailed/k9s@latest
+printf "\tstern:"
 go install github.com/stern/stern@latest
 
-# markdown rendering in terminal
+printf "markdown rendering in terminal with glow:"
 go install github.com/charmbracelet/glow/v2@latest
